02b792526fda3d012f7454ea914ee87e1773982d
Rafa Nonato <rf.nonato@gmail.com>
2017-12-08 21:31:19 -0200

    Incluindo Consign para melhoria na inclusão de Rotas

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var app = require("./config/server");
 
-var rt_home =  require("./app/routes/home.js")(app);
-var rt_users = require("./app/routes/usuarios.js")(app);
-var rt_noticias = require('./app/routes/noticias.js')(app);
-var rt_formulario =  require("./app/routes/formularios.js")(app);
-
-app.listen(3125, function(){
+app.listen(2800, function(){
     console.log("SERVIDOR ON");
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var consign = require('consign');
 var app = express();
 
 app.set('views engine', 'ejs');
 app.set('views' , './app/views');
+
+consign().include('app/routes').into(app);
 
 module.exports = app;
