a22c84d9b372706f8a793283de9835712cbd4954
Rafa Nonato <rf.nonato@gmail.com>
2017-12-08 22:34:32 -0200

    Modularizando formulario de Usuario

--- app/routes/formularios.js
+++ app/routes/formularios.js
@@ -1,5 +1,15 @@
+
 module.exports = (function(app){
+
     app.get('/formularios', function(req, res){
-        res.render("./admin/form_add_noticia.ejs");
+        var conn = app.config.dbConnection();
+
+        conn.query('Select * from formulariousuarios', function(error, result){
+
+        res.render("./admin/form_add_noticia.ejs", {formulariousuarios : result});
+
+         });
+
     });
+
 });
